0bb249385b0ae19dad3c2c12f75ae73b58bfaec9
anroopvullampathi <anroopvullampathi@gmail.com>
2019-05-16 17:48:41 +0530

    16-changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -5,7 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+
+  hedding = ['Image', 'Firstname', 'Lastname', 'Email', 'Number'];
+
+  empname: any = [
+    {img: '../assets/img/icon1.png', fname:'Anroop', lname: 'Vullampathi', email: 'anroop@gmail.com', num: 9849264317},
+    {img: '../assets/img/icon2.png', fname:'Vasu', lname: 'Baggi', email: 'Vasu@gmail.com', num: 9845712567},
+    {img: '../assets/img/icon3.png', fname:'Srikanth', lname: 'Bijjala', email: 'Bijjala@gmail.com', num: 9845712567}
+  ]
 
   constructor() { }
 
