d9e2e22634c67fffa42cd3348219c8421ac54ba0
anroopvullampathi <anroopvullampathi@gmail.com>
2019-05-17 17:10:22 +0530

    color

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent {
 
-  hedding = ['Image', 'Firstname', 'Lastname', 'Email', 'Number'];
+  hedding = ['Image', 'Firstname', 'Lastname', 'Email', 'Number', 'Date'];
 
   empname: any = [
-    {img: '../assets/img/icon1.png', fname:'Anroop', lname: 'Vullampathi', email: 'anroop@gmail.com', num: 9849264317},
-    {img: '../assets/img/icon2.png', fname:'Vasu', lname: 'Baggi', email: 'Vasu@gmail.com', num: 9845712567},
-    {img: '../assets/img/icon3.png', fname:'Srikanth', lname: 'Bijjala', email: 'Bijjala@gmail.com', num: 9845712567}
+    {img: '../assets/img/icon1.png', fname:'Anroop', lname: 'Vullampathi', email: 'anroop@gmail.com', num: 9849264317, date:'h:mm a z'},
+    {img: '../assets/img/icon2.png', fname:'Vasu', lname: 'Baggi', email: 'Vasu@gmail.com', num: 9845712567, date:'h:mm a z'},
+    {img: '../assets/img/icon3.png', fname:'Srikanth', lname: 'Bijjala', email: 'Bijjala@gmail.com', num: 9845712567, date:'h:mm a z'}
   ]
 
   constructor() { }
--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -5,7 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './images.component.html',
   styleUrls: ['./images.component.css']
 })
-export class ImagesComponent implements OnInit {
+export class ImagesComponent {
+  images: any =[
+    {img: 'assets/img/bb1.jpg'},
+    {img: 'assets/img/bb2.jpg'},
+    {img: 'assets/img/a1.jpg'},
+    {img: 'assets/img/a2.jpg'},
+    {img: 'assets/img/a3.jpg'},
+    {img: 'assets/img/a4.jpg'},
+    {img: 'assets/img/g10.jpg'},
+    {img: 'assets/img/g11.jpg'}
+  ]
 
   constructor() { }
 
