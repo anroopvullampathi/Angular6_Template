ae56db95b4acfc19b61bd321ddc37169370d7bec
anroopvullampathi <anroopvullampathi@gmail.com>
2019-05-20 17:18:39 +0530

    table-binding

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,13 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-
+  today: number = Date.now();
   hedding = ['Image', 'Firstname', 'Lastname', 'Email', 'Number', 'Date'];
 
   empname: any = [
-    {img: '../assets/img/icon1.png', fname:'Anroop', lname: 'Vullampathi', email: 'anroop@gmail.com', num: 9849264317, date:'h:mm a z'},
-    {img: '../assets/img/icon2.png', fname:'Vasu', lname: 'Baggi', email: 'Vasu@gmail.com', num: 9845712567, date:'h:mm a z'},
-    {img: '../assets/img/icon3.png', fname:'Srikanth', lname: 'Bijjala', email: 'Bijjala@gmail.com', num: 9845712567, date:'h:mm a z'}
+    {img: '../assets/img/icon1.png', fname:'Anroop', lname: 'Vullampathi', email: 'anroop@gmail.com', num: 9849264317},
+    {img: '../assets/img/icon2.png', fname:'Vasu', lname: 'Baggi', email: 'Vasu@gmail.com', num: 9845712567},
+    {img: '../assets/img/icon3.png', fname:'Srikanth', lname: 'Bijjala', email: 'Bijjala@gmail.com', num: 9845712567}
   ]
 
   constructor() { }
